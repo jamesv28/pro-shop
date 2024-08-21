812c7b3d1d106da369197e0813c9fa5242c596f9
James Volmert <james.volmert@gmail.com>
2024-08-21 17:25:55 -0600

    added footer and header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { Container, Row, Col } from "react-bootstrap";
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="py-3 text-center">
+            <p>ProShop &copy; {currentYear}</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
