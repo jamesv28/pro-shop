466681421b96f27059dfaf5ba3be38e5a467b3a6
James Volmert <james.volmert@gmail.com>
2024-08-21 17:36:41 -0600

    changed asset files and added a logo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-// import './index.css'
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/bootstrap.custom.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
