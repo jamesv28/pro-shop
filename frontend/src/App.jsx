de164f067d7d10b279d40f73415a9f1598598c41
James Volmert <james.volmert@gmail.com>
2024-08-22 16:18:10 -0600

    more ui setup and installed react-router-bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Header />
       <main>
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/bootstrap.custom.css";
+import HomeScreen from "./screens/HomeScreen.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
