5dd3bb50aed7ebd39d17fa837423aa113d620117
James Volmert <james.volmert@gmail.com>
2024-08-22 16:42:17 -0600

    rating component and added style to product title

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 import PropTypes from "prop-types";
 
 const Product = ({ product }) => {
@@ -10,10 +11,16 @@ const Product = ({ product }) => {
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
