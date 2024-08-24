3305d28585e67d6c59ddd832d2bd8ff9450eb841
James Volmert <james.volmert@gmail.com>
2024-08-23 19:49:12 -0600

    got mongo setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
 
+dotenv.config();
+
+connectDb();
 const port = process.env.PORT || 5001;
-
-console.log("process", process.env.PORT);
 
 const app = express();
 
