e1320351f8d5237a6f1e76eb06073ae543e4d9a0
James Volmert <james.volmert@gmail.com>
2024-08-27 17:40:08 -0600

    added redux toolkit for getting products by id

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,8 @@
-export const BASE_URL =
-  process.env.NODE_ENV == "development"
-    ? "http://localhost:5001"
-    : "https://www.example.com";
+// export const BASE_URL =
+//   process.env.NODE_ENV == "development"
+//     ? "http://localhost:5001"
+//     : "https://www.example.com";
+export const BASE_URL = "";
 export const PRODUCT_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const URDERS_URL = "/api/orders";
