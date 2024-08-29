4b0903b052a3d36dacb7c3433916348e984e0e65
James Volmert <james.volmert@gmail.com>
2024-08-28 18:43:36 -0600

    added badge for shopping cart

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,12 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-};
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -25,22 +23,7 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item];
       }
 
-      // calculate items price
-      state.itemsPrice = state.cartItems.reduce((acc, item) =>
-        addDecimals(acc + item.price * item.qty, 0)
-      );
-      // calculate shipping price
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-      // calculate tax price 15%
-      state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2));
-      // calculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
