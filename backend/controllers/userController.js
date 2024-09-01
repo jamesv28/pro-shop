56068ff22a23cebabc2ead5097b100f8e45ec540
James Volmert <james.volmert@gmail.com>
2024-09-01 17:39:48 -0600

    setup jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middleware/asyncHandler.js";
-
+import jwt from "jsonwebtoken";
 // @desc get users and get token
 // @route GET /api/users/login
 // @access PUBLIC
@@ -9,6 +9,19 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
     res.json({
       _id: user._id,
       name: user.name,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,6 +29,7 @@ const userSchema = mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
