29cc76e7b1450f4f5e58b8b5d78a4d9776caa709
James Volmert <james.volmert@gmail.com>
2024-09-03 17:54:37 -0600

    worked on slices and login screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,7 @@ import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
 import CartScreen from "./screens/CartScreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
 import "./index.css";
 import "./assets/bootstrap.custom.css";
 
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
