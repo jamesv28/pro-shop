6161060441065aa8b5b14386014147c6596004c6
James Volmert <james.volmert@gmail.com>
2024-09-05 19:58:50 -0600

    checkout step component

---
+++ frontend/src/components/CheckoutSteps.jsx
@@ -0,0 +1,54 @@
+import PropTypes from "prop-types";
+import { Nav } from "react-bootstrap";
+import { Link } from "react-router-bootstrap";
+
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+    <Nav className="justify-content-center mb-4">
+      <Nav.Item>
+        {step1 ? (
+          <Nav.Link as={Link} to="/login">
+            Sign In
+          </Nav.Link>
+        ) : (
+          <Nav.Link disabled>Sign In</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step2 ? (
+          <Nav.Link as={Link} to="/shipping">
+            Shipping
+          </Nav.Link>
+        ) : (
+          <Nav.Link disabled>Shipping</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step3 ? (
+          <Nav.Link as={Link} to="/payment">
+            Payment
+          </Nav.Link>
+        ) : (
+          <Nav.Link disabled>Payment</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step4 ? (
+          <Nav.Link as={Link} to="/placeorder">
+            Place Order
+          </Nav.Link>
+        ) : (
+          <Nav.Link disabled>Place Order</Nav.Link>
+        )}
+      </Nav.Item>
+    </Nav>
+  );
+};
+
+CheckoutSteps.propTypes = {
+  step1: PropTypes.bool,
+  step2: PropTypes.bool,
+  step3: PropTypes.bool,
+  step4: PropTypes.bool,
+};
+export default CheckoutSteps;
