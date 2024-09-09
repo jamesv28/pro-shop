8047daee77f43485674a62dbd8388ffc20c5661c
James Volmert <james.volmert@gmail.com>
2024-09-09 11:19:38 -0600

    started paypal backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -65,10 +65,26 @@ const getOrderById = asyncHandler(async (req, res, next) => {
 });
 
 // @desc update order to paid
-// @route GET /api/orders/:id/paid
+// @route PUT /api/orders/:id/paid
 // @access Private
 const updateOrderToPaid = asyncHandler(async (req, res, next) => {
-  res.send("update order to paid");
+  const order = Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentRequest = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.email_address,
+    };
+    const updateOrder = await order.save();
+    res.status(200).json(updateOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc update order to delivered
